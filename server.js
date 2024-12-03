503497f64450bae2b7172ae1488879d4fc8834ea
Mubbashir Bawany <49834974+MUBBASHIRBAWANY@users.noreply.github.com>
2024-12-03 08:22:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const http = require ("http")
 const app = require("./app")
 const port = process.env.PORT 
-const server = http.createServer(app)
+
 const connectDb = require('./db/db.js')
 
  connectDb()
 
-server.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log(`server runing at ${port}`)
 })
