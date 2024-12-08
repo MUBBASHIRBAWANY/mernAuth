5778bfd3191b01b21a8b92cd798025729d30ad56
Mubbashir Bawany <49834974+MUBBASHIRBAWANY@users.noreply.github.com>
2024-12-07 22:26:49 -0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const connectDb = mongoose.connect('mongodb+srv://Mubbashir123:Mubbashir1234@airbnb1.873o2.mongodb.net/uber-clone').then(()=>{
+const connectDb = mongoose.connect('check').then(()=>{
     console.log("DataBase Conected")
 })
 
