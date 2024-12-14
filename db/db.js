fde33c1ba2ac4781548b9a2868d72d8f7ba7a3f2
Mubbashir Bawany <49834974+MUBBASHIRBAWANY@users.noreply.github.com>
2024-12-14 10:32:14 -0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 const URL = process.env.URL
 
 const db = () => {
- mongoose.connect(URL).then(()=>{
+ mongoose.connect("mongodb+srv://Mubbashir123:Mubbashir1234@airbnb1.873o2.mongodb.net/uber-clone").then(()=>{
     console.log("DataBase Conected")
 })
 }
