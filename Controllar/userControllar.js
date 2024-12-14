404ab361989f4995b112aeb1cf3235b741eba868
MUBBASHIRBAWANY <mubbashirbawany@gmail.com>
2024-12-14 09:49:22 -0800

    versol Add

--- Controllar/userControllar.js
+++ Controllar/userControllar.js
@@ -1,74 +1,86 @@
 import mongoose from "mongoose";
-import userSchema, { getAuthontication , comparePassword } from "../Models/userModel.js";
-import  jwt  from "jsonwebtoken";
-import bcrypt from 'bcrypt'
+import userSchema, { getAuthontication } from "../Models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export const userRegister = async (req,res) =>{
-    const {name, email, password, image} = req.body
-    if(!name ||!email ||!password ||!image) return res.status(400).send("All fields are required")
+const User = userSchema
 
-    const hashPassword = await userSchema.hashPassword(password)
-    
-    let val = await userSchema.create({
-        name : name,
-        email : email,
-        password : hashPassword,
-        imageUrl : image,
-        
-    })
-    const  token = await getAuthontication(val)
- res.send("hogia")
-}
-
-
-export const userProfile = async (req,res)=>{
-    const check1 = req.rawHeaders[1]
-     const token = req.cookies.token || check1.split(' ')[1]
-    if(!token){
-        return res.status(403).send('Invalid Token')
-        
-    }
-    
-    jwt.verify(token, process.env.JWT_SECRET, (err)=>{
-        console.log(err)
-        if(err){
-            res.status(403).send('Invalid Token1')
-        }   
-        else{
-            const user = jwt.decode(token, process.env.JWT_SECRET)
-            
-            res.send({status : true, data: user})
-        }
-    })
-}
-
-
-
-export const userLogin = async (req,res)=>{
-    const {email, password} = req.body
-      if(!email ||!password) return res.status(400).send("All fields are required")
-    
-    const user = await userSchema.findOne({email}).select('+password')
-    
-    if(!user) return res.status(400).send("Invalid email or password")
-    
-    const validPassword = await bcrypt.compare(password, user.password)
-    if(validPassword == true){
-        const  token = await getAuthontication(user)
-        res.cookie('token', token)
-        res.send("true")
-    }
-    else{
-        res.status(400).send("Invalid email or password")
-    }
-}
-
-export const userLogout = (req,res) =>{
+export const userRegister = async (req, res) => {
     try {
-        res.clearCookie('token')
-        res.send("Logged Out")
-    }catch(err){
-        res.send("some thing went wrong")
+        const { name, email, password, image } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).send("All fields are required");
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashPassword,
+            imageUrl: image,
+        });
+
+        const token = await getAuthontication(newUser);
+        res.status(201).send({ message: "Registration successful", token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(`Server Error: ${err.message}`);
     }
-}
+};
+
+export const userProfile = async (req, res) => {
+    try {
+        const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.status(403).send("Invalid Token");
+        }
+
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).send("Invalid Token");
+            }
+            res.status(200).send({ status: true, data: decoded });
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(`Server Error: ${err.message}`);
+    }
+};
+
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send("All fields are required");
+        }
+
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(400).send("Invalid email or password");
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            return res.status(400).send("Invalid email or password");
+        }
+
+        const token = await getAuthontication(user);
+        res.cookie("token", token);
+        res.status(200).send("Login successful");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(`Server Error: ${err.message}`);
+    }
+};
+
+export const userLogout = (req, res) => {
+    try {
+        res.clearCookie("token");
+        res.status(200).send("Logged Out");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(`Something went wrong: ${err.message}`);
+    }
+};
